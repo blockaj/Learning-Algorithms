2c20c0698d55457d2da271fac6edc4e368d8c56e
Aaron <ajblock@gmail.com>
2014-08-11 10:25:57 -0400

    Same old insertion sort, same old c++

---
+++ insertion_sort/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int* insertionSort(int* a, int inputLength);
+
+int main(int argc, char *argv[]) {
+	cout << "Type in array length: ";
+	int arrayLength;
+	cin >> arrayLength;
+	int inputArray[arrayLength];
+	for (int i = 0; i < arrayLength; i++) {
+		int input = rand() % arrayLength + 1;
+		inputArray[i] = input;
+	}
+	int* orderedList = insertionSort(inputArray, arrayLength);
+	for(int i = 0; i < arrayLength; i++) {
+		cout << orderedList[i] << endl;
+	}
+	return 0;
+}
+
+int* insertionSort(int* a, int inputLength) {
+	for(int j = 1; j < inputLength; j++) {
+		int key = a[j];
+		int i = j - 1;
+		while(i > -1 && a[i] > key) {
+			a[i + 1] = a[i];
+			i = i - 1;
+		}
+		a[i + 1] = key;
+	}
+	return a;
+}
